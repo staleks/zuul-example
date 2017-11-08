7050a8a350fdb76a7232fcc39e0aa766150146b9
staleks <staleks@gmail.com>
2017-11-08 10:53:24 +0100

    refactor

--- export-service/src/main/java/com/jatheon/zuul/export/controller/ExportController.java
+++ export-service/src/main/java/com/jatheon/zuul/export/controller/ExportController.java
@@ -2,6 +2,7 @@ package com.jatheon.zuul.export.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ public class ExportController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExportController.class);
 
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
     @GetMapping("/hello")
     public String handle() {
-        return "Hello world!";
+        return greetingMessage;
     }
 
 }
